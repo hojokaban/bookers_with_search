e4797db78630e6372c5fd71626c900e691b9c1da
hojokaban <hojokaban@gmail.com>
2020-04-10 19:18:09 +0900

    [Start]5-9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+
+	def home
+	end
+
+	def new
+	end
+
+	def index
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+end
