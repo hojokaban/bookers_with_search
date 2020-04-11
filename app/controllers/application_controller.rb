d7ff3f066250f8fdb89491ff9136dae361cddfad
hojokaban <hojokaban@gmail.com>
2020-04-11 21:50:03 +0900

    [5-9]edit is completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
 	protected
 	def configure_permitted_parameters
-		added_attrs = [:name, :introduction, :profile_image_id]
-		devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
-		devise_parameter_sanitizer.permit(:sign_in, keys: added_attrs)
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
 	end
 end
