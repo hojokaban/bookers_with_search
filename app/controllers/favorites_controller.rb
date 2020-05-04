b00cf4b2488c21e29974a5a2095711f78f7914b2
hojokaban <hojokaban@gmail.com>
2020-05-04 16:56:52 +0900

    Function create action in favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,10 @@
 class FavoritesController < ApplicationController
 	
 	def create
+		#binding.pry
+		book = Book.find(params[:book_id])
+		Favorite.new(book_id: book.id,
+			         user_id: current_user.id).save
 		redirect_to books_path
 	end
 
