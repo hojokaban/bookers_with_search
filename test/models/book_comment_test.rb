8eef71e7e1f41ea61cfff3dd7f66914aa998144d
hojokaban <hojokaban@gmail.com>
2020-05-05 11:09:38 +0900

    Add commnet model test

--- test/models/book_comment_test.rb
+++ test/models/book_comment_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class BookCommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@book = books(:book1)
+		@user = users(:luffy)
+		@comment = BookComment.new(content:"example", user_id: @user.id, book_id: @book.id)
+	end
+
+	test "should be valid" do
+		assert @comment.valid?
+	end
 end
