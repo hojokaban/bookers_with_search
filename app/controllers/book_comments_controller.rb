b8c5571d2ebd57e1beaec766152b0227de735d03
hojokaban <hojokaban@gmail.com>
2020-05-05 12:35:06 +0900

    Modify comment form

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,6 +1,11 @@
 class BookCommentsController < ApplicationController
 
 	def create
+		if BookComment.new(user_id: current_user.id, book_id: params[:id]).save
+			flash[:notice] = "successfully upload comment!"
+		else
+			redirect_to book_path(params[:id])
+		end
 	end
 
 	def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 	get 'home/about' => 'books#about', as: 'about_book'
 	post 'favorites/:id' => 'favorites#create', as: 'favorite'
 	delete 'favorites/:id' => 'favorites#destroy', as: 'favorites'
+	post 'comments/:id' => 'book_comments#create', as: 'comment'
+	delete 'comments/:id' => 'book_comments#destroy', as: 'comments'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
