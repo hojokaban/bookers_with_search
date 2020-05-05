22bc8725b6b8a31f16a3f10fef298d8989f6e5b9
hojokaban <hojokaban@gmail.com>
2020-05-06 00:29:30 +0900

    Make relationships and Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", 
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attachment :profile_image
 
   validates :name, length: {in: 2..20}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_015854) do
+ActiveRecord::Schema.define(version: 2020_05_05_150926) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "content"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2020_05_05_015854) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["book_id"], name: "index_favorites_on_book_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
