6ce06e3565a17be93b18e76ebd6b9f3416cdb554
hojokaban <hojokaban@gmail.com>
2020-05-06 13:34:32 +0900

    Add follow_button test

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,18 +4,19 @@ class FollowingTest < ActionDispatch::IntegrationTest
 	include Warden::Test::Helpers
 
   def setup
-  	@user = users(:luffy)
-  	@other_user = users(:zoro)
-  	login_as(@user, :scope => :user)
+  	@luffy = users(:luffy)
+  	@zoro = users(:zoro)
+  	@sanji = users(:sanji)
+  	login_as(@luffy, :scope => :user)
   end
 
   test "should see following/followers count" do
 
   	get users_path
-  	assert_match @user.following.count.to_s, response.body
-  	assert_match @user.followers.count.to_s, response.body
-  	assert_select "a[href=?]", following_user_path(@user)
-  	assert_select "a[href=?]", followers_user_path(@user)
+  	assert_match @luffy.following.count.to_s, response.body
+  	assert_match @luffy.followers.count.to_s, response.body
+  	assert_select "a[href=?]", following_user_path(@luffy)
+  	assert_select "a[href=?]", followers_user_path(@luffy)
   	assert_match "1 following", response.body
   	assert_match "1 follower", response.body
 
@@ -23,20 +24,38 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
   test "following page" do
 
-  	get following_user_path(@user)
+  	get following_user_path(@luffy)
   	assert_select "h2", "User Following"
-  	assert_match @user.following.first.name, response.body
+  	assert_match @luffy.following.first.name, response.body
   	assert_match "1 following", response.body
 
   end
 
   test "followers page" do
 
-  	get followers_user_path(@user)
+  	get followers_user_path(@luffy)
   	assert_select "h2", "User Followers"
-  	assert_match @user.followers.first.name, response.body
+  	assert_match @luffy.followers.first.name, response.body
   	assert_match "1 follower", response.body
 
+  end
+
+  test "follow button" do
+
+  	get user_path(@luffy)
+  	assert_select "span.btn-parimary", count: 0
+  	assert_select "span.btn-success", count: 0
+  	assert_select "input.btn-primary", count: 1
+
+  	get user_path(@zoro)
+  	assert_select "span.btn-primary", count: 1
+  	assert_select "span.btn-success", count: 0
+  	assert_select "input.btn-primary", count: 1
+
+  	get user_path(@sanji)
+  	assert_select "span.btn-primary", count: 0
+  	assert_select "span.btn-success", count: 1
+  	assert_select "input.btn-primary", count: 1
   end
 
 end
